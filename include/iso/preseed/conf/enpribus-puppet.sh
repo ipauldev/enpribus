d53f91a7a5acebe9b882acb09d225a1c64eb5e9c
Paul Hassinger <hassinger.paul@ipaul.com>
2012-07-17 15:58:33 -0500

    Added puppet client to puppet master and started configuration of puppet

--- include/iso/preseed/conf/enpribus-puppet.sh
+++ include/iso/preseed/conf/enpribus-puppet.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 USERNAME=$1
-SSHKEY=$2
+SSH_KEY=$2
 RELEASE=$3
 
-#Update SSH Public Key
+DNS_HOST_NAME=`hostname`
+DNS_DOMAIN_NAME=`dnsdomainname`
+
+#Update SSH public key
 mkdir /home/${USERNAME}/.ssh/
-echo "${SSHKEY}" > /home/${USERNAME}/.ssh/authorized_keys
+echo "${SSH_KEY}" > /home/${USERNAME}/.ssh/authorized_keys
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.ssh/
 chmod -R u=rwX,go= /home/${USERNAME}/.ssh/
 
-#setup Puppet Master
+#upgrade Puppet master to official repo
+cd /tmp
 wget http://apt.puppetlabs.com/puppetlabs-release-${RELEASE}.deb
 dpkg -i puppetlabs-release-precise.deb
 rm puppetlabs-release-precise.deb
@@ -18,3 +22,9 @@ rm puppetlabs-release-precise.deb
 #upgrade packages
 apt-get update
 apt-get -y upgrade
+
+#configure Puppet
+echo -e "server=${DNS_HOST_NAME}.${DNS_DOMAIN_NAME}\ndns_alt_names=${DNS_HOST_NAME},puppet.${DNS_DOMAIN_NAME}\n\n[agent]\nserver=${DNS_HOST_NAME}.${DNS_DOMAIN_NAME}" >> /etc/puppet/puppet.conf
+/etc/init.d/puppet stop
+/etc/init.d/puppetmaster stop
+rm -Rf /var/lib/puppet/ssl/
